b317203fd5a08b626e7a88ad242f3503babccbef
CARLA-ANTONIA OLTEANU <carla.olteanu@stud.ubbcluj.ro>
2023-10-22 22:39:59 +0300

    Add project files.

---
+++ Data/Olteanu_Carla_Antonia_Lab2Context.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Olteanu_Carla_Antonia_Lab2.Models;
+
+namespace Olteanu_Carla_Antonia_Lab2.Data
+{
+    public class Olteanu_Carla_Antonia_Lab2Context : DbContext
+    {
+        public Olteanu_Carla_Antonia_Lab2Context (DbContextOptions<Olteanu_Carla_Antonia_Lab2Context> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Olteanu_Carla_Antonia_Lab2.Models.Book> Book { get; set; } = default!;
+
+        public DbSet<Olteanu_Carla_Antonia_Lab2.Models.Publisher>? Publisher { get; set; }
+
+        public DbSet<Olteanu_Carla_Antonia_Lab2.Models.Author>? Authors { get; set; }
+    }
+}
---
+++ Models/Author.cs
@@ -0,0 +1,12 @@
+﻿namespace Olteanu_Carla_Antonia_Lab2.Models
+{
+    public class Author
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public ICollection<Book>? Books { get; set; }
+    }
+}
+
